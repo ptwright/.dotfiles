0aa0fd9a773bdecf8cc8a05030437a47f0e88123
Paul Wright <paulwright@proton.me>
2026-02-08 08:54:09 -0700

    fish, rust

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,11 +6,10 @@ end
 
 set -gx PATH $PATH /opt/nvim
 
-
 # BEGIN opam configuration
 # This is useful if you're using opam as it adds:
 #   - the correct directories to the PATH
 #   - auto-completion for the opam binary
 # This section can be safely removed at any time if needed.
-test -r '/home/ptw/.opam/opam-init/init.fish' && source '/home/ptw/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
+# test -r '/home/ptw/.opam/opam-init/init.fish' && source '/home/ptw/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
 # END opam configuration
